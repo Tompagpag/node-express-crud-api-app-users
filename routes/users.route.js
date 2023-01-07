92853547f0c3adf1dc8af20fdf50638b15cfde0c
Tom Paganel <paganel.tom@gmail.com>
2023-01-07 16:46:43 +0100

    code find action, works

--- routes/users.route.js
+++ routes/users.route.js
@@ -10,14 +10,15 @@ router.get("/list", (req, res) => {
 
 router.get("/find/:id", (req, res) => {
   console.log(`La route ${req.originalUrl} fonctionne`);
-  // try {
-
-  // } catch (error) {
-  //   res.status(error.status).json({
-  //     msg: error.message,
-  //     code: error.code,
-  //   });
-  // }
+  try {
+    const id = parseInt(req.params.id, 10);
+    res.json(UserService.getUser(id));
+  } catch (error) {
+    res.status(error.status).json({
+      msg: error.message,
+      code: error.code,
+    });
+  }
 });
 
 router.post("/create", (req, res) => {
--- services/users.service.js
+++ services/users.service.js
@@ -18,6 +18,11 @@ class UserService {
   static list() {
     return users;
   }
+
+  static getUser(id) {
+    const user = users.find((user) => user.id === id);
+    return user;
+  }
 }
 
 export default UserService
